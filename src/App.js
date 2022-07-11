444e4c4f79ca7c4860e102fb9ad644ff48f097e8
helioramon <helio.alvareng@gmail.com>
2022-07-11 03:10:58 -0300

    rededering a state

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    nome: ['Hélio'],
+    idade: 27,
+    comida: 'Lasanha',
+    músicas: ['Cátia de França - Djaniras', 'Solange - Scales (feat. Kelela)', 'Led zeppelin - Since Ive Been Loving You']
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.nome}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comida}</h3>
+        <ul>
+          <ol>{this.state.músicas[0]}</ol>
+          <ol>{this.state.músicas[1]}</ol>
+          <ol>{this.state.músicas[2]}</ol>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
